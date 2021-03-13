7aa1fba4542a390588009ea54786968983f80349
estein1988 <estein1988@gmail.com>
2021-03-13 14:29:21 -0700

    Add dogs data in JSON file and fetch it from store and render to Card component.

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,59 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <div>VIEWS GET THE STATE, PASSES IT DOWN TO COMPONENTS</div>
+    <h1>PupFest 2020</h1>
+    <ul class="dogs">
+      <li v-for="dog in dogs" :key="dog.id">
+        <DogCard :dog="dog"/>
+      </li>
+    </ul>
+    <!-- <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
-    <router-view/>
+    <router-view/> -->
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+import DogCard from "@/components/DogCard"
+  export default {
+    components: {
+      DogCard,
+    },
+    computed: {
+      dogs() {
+        return this.$store.state.dogs
+      }
+    },
+    mounted(){
+      this.$store.dispatch("fetchDogs")
     }
   }
-}
+</script>
+
+<style lang="scss">
+  ul {
+    list-style: none;
+  }
+// #app {
+//   font-family: Avenir, Helvetica, Arial, sans-serif;
+//   -webkit-font-smoothing: antialiased;
+//   -moz-osx-font-smoothing: grayscale;
+//   text-align: center;
+//   color: #2c3e50;
+// }
+
+// #nav {
+//   padding: 30px;
+
+//   a {
+//     font-weight: bold;
+//     color: #2c3e50;
+
+//     &.router-link-exact-active {
+//       color: #42b983;
+//     }
+//   }
+// }
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -5,10 +5,19 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    dogs: []
   },
   mutations: {
+    setDogs(state, dogs){
+      state.dogs = dogs
+    },
   },
   actions: {
+    fetchDogs({commit}){
+      fetch("./dogs.json")
+        .then(response => response.json())
+        .then(response => commit("setDogs", response.dogs))
+    }
   },
   modules: {
   }
